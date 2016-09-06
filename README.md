99d2e016ab38a3973c2afde855e318d9c45b3e7c
Craig Yamato <simmer006@gmail.com>
2016-09-06 18:59:18 +0000

    added ubuntu 14.04 to setup.sh and other clean up

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # chfagent-systemd
-Kentik Proxy Agent startup script for systemd
+Kentik Proxy Agent startup script for systemd on RHEL/CentOS v7, Debian v8, & Ubuntu v14.04
 
-To set the Proxy Agent to start on boot use the command =  systemctl enable chfagent
+This script will install wget & chfagent if needed using the local standard package manager (yum/apt)  
+It will create the needed SystemD files and local proxy configuration and make calls to systemctl to install the SystemD chfagent scripts at boot up.  This configuration will also set chfagent to restart it self in case of failure every 60s.  Please note that if you do not curently have nay devices configured the Proxy Agent will fail and restart every 60s until there is atleast one device configured in the Kentik Portal.  
 
-To start the Proxy Agent use the command "systemctl start chfagent"
+This script is offed as is with out warenty or guranty of any kind under a GPLv3 licnese is open for modifacation and reuse as needed.  We ask that any changes which may be benfical to others be posted to the Github for inclustion.  
 
-To stop the Proxy Agent use the command "systemctl stop chfagent"
+The local config file is located at /etc/systemd/system/chfagent.service.d/local.conf  
+chfagent is in /usr/bin/chfagent  
 
-To restart the Proxy Agent use the command "systemctl restart chfagent"
+Many thanks!
 
-Local Config File = /etc/systemd/system/chfagent.service.d/local.conf
+to download and excute this script please use the command wget https://raw.githubusercontent.com/cyamato/chfagent-systemd/master/setup.sh && sudo chmod +x ./setup.sh && sudo ./setup.sh  
 
-wget https://raw.githubusercontent.com/cyamato/chfagent-systemd/master/setup.sh && sudo chmod +x ./setup.sh && sudo ./setup.sh
+To manualy control the proxy:  
+* To start the Proxy Agent use the command "systemctl start chfagent"  
+* To stop the Proxy Agent use the command "systemctl stop chfagent"  
+* To restart the Proxy Agent use the command "systemctl restart chfagent"
--- setup.sh
+++ setup.sh
@@ -1,74 +1,132 @@
 #!/bin/bash
-# Setup chfagent - The Kentik Proxy Agent on CentOS/RHEL
-# Auther: Craig Yamato 2
+# Setup chfagent - The Kentik Proxy Agent with SystemD
+# Auther: Craig Yamato II
 # date: 08/31/2016
-# url: 
-# GNU
+# url: https://github.com/cyamato/chfagent-systemd
+# License: GPLv3
 
-if [[ -e "/etc/redhat-release" ]]; then
-
-if [[ $(grep -Eom1 '[0-9]' /etc/redhat-release | grep -Eom1 [0-9]) == "7" ]]; then
-chfagent_dir="/etc/systemd/system/"
+# setup common vars
 proxy_Local="/usr/bin/chfagent"
-proxy_dl_url="https://kentik.com/packages/builds/rhel/7/chfagent_rhel_7-latest-1.x86_64.rpm"
-
-else
-echo "Sorry only verson 7 of RHEL/CentOS is supported by this script"
-fi
-
-else
-
-if [[ -e "/etc/os-release" ]]; then
-
-if [[ $(grep -Eom1 '[0-9]' /etc/os-release | grep -Eom1 [0-9]) == "8" ]]; then
-chfagent_dir="/lib/systemd/system/"
-proxy_Local="/usr/bin/chfagent"
-proxy_dl_url="https://kentik.com/packages/builds/debian/8/chfagent-jessie_latest_amd64.deb"
-
-else
-echo "Sorry only version 8 of Debian is supported by this script"
-fi
-
-fi
-fi
-
 url_chfagentServic="https://raw.githubusercontent.com/cyamato/chfagent-systemd/master/chfagent.service"
 
+# Make sure user is running with root
 if [ ! "$(whoami)" == 'root' ]; then
-echo "This script must be executed as root or with sudo"
-exit 0
+    echo "This script must be executed as root or with sudo"
+    exit 0
 fi
+
+# Check for dependancies
+echo "Checking for dependancies"
+
+# wget
+wgetCheck=1
+if [[ $(hash wget) ]]; then 
+    echo "Your system does not have wget.  We will call your package manager to install it for you"
+    wgetCheck=0
+fi
+
+# chfagent
+chfagentCheck=1
+if [[ ! -e $proxy_Local ]]; then
+    echo "Kentik Proxy Agent not found on system.  We will call your package manager to install it for you"
+    chfagentCheck=0
+fi
+
+# check which OS they are running
+
+# Redhat / CentOS
+if [[ -e "/etc/redhat-release" ]]; then
+
+    if [[ $(grep -Eom1 '[0-9]' /etc/redhat-release | grep -Eom1 '[0-9]') == "7" ]]; then
+        OS="rhel"
+        systemd_dir="/etc/systemd/system/"
+        proxy_dl_url="https://kentik.com/packages/builds/rhel/7/chfagent_rhel_7-latest-1.x86_64.rpm"
+        pakageName="chfagent_rhel_7-latest-1.x86_64.rpm"
+        
+        if [[ $wgetCheck == 0 ]]; then
+            yum install wget
+        fi
+        
+        if [[ $chfagentCheck == 0 ]]; then 
+            wget $proxy_dl_url
+            yum install chfagent_rhel_7-latest-1.x86_64.rpm
+        fi
+    else
+        echo "Sorry only verson 7 of RHEL/CentOS is supported by this script"
+    fi
+
+# Debian
+else
+
+    if [[ -e "/etc/os-release" ]]; then
+    
+        if [[ $(grep -Eom1 'NAME="Debian' /etc/os-release) == 'NAME="Debian' ]]; then
+        
+            if [[ $(grep -Eom1 'VERSION="[0-9]?[0-9]?.?[0-9]?[0-9]' /etc/os-release | grep -Eom1 '[0-9][0-9]?.?[0-9]?[0-9]?') == "8" ]]; then
+            
+                OS="debian"
+                systemd_dir="/lib/systemd/system/"
+                proxy_Local="/usr/bin/chfagent"
+                proxy_dl_url="https://kentik.com/packages/builds/debian/8/chfagent-jessie_latest_amd64.deb"
+                packageName="chfagent-jessie_latest_amd64.deb"
+                
+                if [[ $wgetCheck == 0 ]]; then
+                    apt-get install wget
+                fi
+            
+                if [[ $chfagentCheck == 0 ]]; then 
+                    wget $proxy_dl_url
+                    apt-get install chfagent-jessie_latest_amd64.deb
+                fi
+            
+            else
+                echo "Sorry only version 8 of Debian is supported by this script"
+            fi
+        
+        else
+        
+            if [[ $(grep -Eom1 'NAME="Ubuntu' /etc/os-release) == 'NAME="Ubuntu' ]]; then
+                if [[ $(grep -Eom1 'VERSION="[0-9]?[0-9]?.?[0-9]?[0-9]' /etc/os-release | grep -Eom1 '[0-9][0-9]?.?[0-9]?[0-9]?') == "14.04" ]]; then
+                
+                    OS="ubuntu"
+                    systemd_dir="/lib/systemd/system/"
+                    proxy_Local="/usr/bin/chfagent"
+                    proxy_dl_url="https://kentik.com/packages/builds/ubuntu/14.04/chfagent-trusty_latest_amd64.deb"
+                    packageName="chfagent-trusty_latest_amd64.deb"
+                    
+                    if [[ $wgetCheck == 0 ]]; then
+                        apt-get install wget
+                    fi
+                
+                    if [[ $chfagentCheck == 0 ]]; then 
+                        wget $proxy_dl_url
+                        apt-get install chfagent-trusty_latest_amd64.deb
+                    fi
+                        
+                else
+                    echo "Sorry only version 14.04 of Ubuntu is supported by this script"
+                fi
+            fi
+        fi
+    else
+        echo "Sorry this linux distrobution and or version is not curently supported by this script"
+    fi
+fi
+
+# Install Proxy Systemd Scripts and configuration file
+localFile="${systemd_dir}chfagent.service"
 
 echo ""
-echo "Now setting up the Kentik Proxy Agent - chfagent"
-
-if [[ $(hash wget) ]]; then 
-echo "Your system does not have wget.  We will call your package manager to install it for you"
-yum install wget
-fi
-
-read -r -p "Have you created any devices in the Kentik Portal from which you will be sending flow? [y/N] " response
-response=${response,,}
-if [[ ! $response =~ ^(yes|y)$ ]]; then
-echo "In the Kentik Portal Admin>Devices menu please add your network devices which you will be sending flow data from and then run this script again (chfagent-setup.sh)"
-exit 0
-fi
-
-read -r -p "Have you created a user for the Proxy Agent in the Kentik Portal? [y/N] " response
-response=${response,,}
-if [[ ! $response =~ ^(yes|y)$ ]]; then
-echo "In the Kentik Portal Admin>Users menu please add a user account which the Proxy Agent should use and then run this script again (chfagent-setup.sh)"
-exit 0
-fi
+echo "Now setting up the Kentik Proxy Agent, chfagent, SystemD Scripts"
 
 while [[ -z "${emailAddress// }" ]]
 do
-read -r -p "Please enter the email address for the account the Kentik Proxy Agent will use: " emailAddress
+    read -r -p "Please enter the email address for the account the Kentik Proxy Agent will use: " emailAddress
 done
 
 while [[ -z "${apiKey// }" ]]
 do
-read -r -p "Please enter the accounts API Key: " apiKey
+    read -r -p "Please enter the accounts API Key: " apiKey
 done
 
 echo "Your server is curently configured with the following IP(s):"
@@ -77,40 +135,30 @@ ifconfig | awk -F "[: ]+" '/inet / { print $3 }'
 read -r -p "Please enter the ip address Kentik Proxy Agent will use, with the IP address 0.0.0.0 the proxy will be accessabule on all IPs used by this server [0.0.0.0]" ip
 
 if [[ -z "${ip// }" ]]; then
-ip="0.0.0.0"
+    ip="0.0.0.0"
 fi
-
-if [[ ! -e $proxy_Local ]]; then
-echo "Kentik Proxy Agent not found on system.  Installing it now"
-wget $proxy_dl_url
-yum install chfagent_rhel_7-latest-1.x86_64.rpm
-fi
-
-localFile="${chfagent_dir}chfagent.service"
 
 if [[ -e $localFile ]]; then
-rm $localFile
+    rm $localFile
 fi
 
 wget -O $localFile $url_chfagentServic
 
-if [ ! -d "${chfagent_dir}chfagent.service.d/" ]; then
-mkdir ${chfagent_dir}chfagent.service.d/
+if [ ! -d "${systemd_dir}chfagent.service.d/" ]; then
+    mkdir ${systemd_dir}chfagent.service.d/
 fi
 
-if [ -e "${chfagent_dir}chfagent.service.d/local.conf" ]; then
- rm ${chfagent_dir}chfagent.service.d/local.conf
+if [ -e "${systemd_dir}chfagent.service.d/local.conf" ]; then
+    rm ${systemd_dir}chfagent.service.d/local.conf
 fi
 
-echo "[Service]" >> ${chfagent_dir}chfagent.service.d/local.conf
-echo "Environment='chfagent_email=$emailAddress'" >> ${chfagent_dir}chfagent.service.d/local.conf
-echo "Environment='chfagent_token=$apiKey'" >> ${chfagent_dir}chfagent.service.d/local.conf
-echo "Environment='chfagent_ip=$ip'" >> ${chfagent_dir}chfagent.service.d/local.conf
+echo "[Service]" >> ${systemd_dir}chfagent.service.d/local.conf
+echo "Environment='chfagent_email=$emailAddress'" >> ${systemd_dir}chfagent.service.d/local.conf
+echo "Environment='chfagent_token=$apiKey'" >> ${systemd_dir}chfagent.service.d/local.conf
+echo "Environment='chfagent_ip=$ip'" >> ${systemd_dir}chfagent.service.d/local.conf
 
-rm ./chfagent.service
-
-if [[ -e "chfagent_rhel_7-latest-1.x86_64.rpm" ]]; then
-rm ./chfagent_rhel_7-latest-1.x86_64.rpm
+if [[ -e "$packageName" ]]; then
+    rm $packageName
 fi
 
 eval systemctl enable chfagent
